b92ed020c98f7b355a8cf582d008bdc2497e7af5
Steel Cooper <steel.cooper@proton.me>
2025-07-19 02:38:17 +1000

    initialize repo and add nvim dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.options")
+require("config.lazy")
+require("lazy").setup("plugins")
+require("config.colour")
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.opt.clipboard = 'unnamedplus'
+
+-- Interface
+vim.opt.number = true
+-- vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+--Tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
