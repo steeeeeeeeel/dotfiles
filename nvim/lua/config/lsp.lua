2c236376f0da59de90ae73cc68e1b232084b1fc3
steeeeeeeeel <steel.cooper@proton.me>
2026-08-28 16:18:36 +1000

    Add mason-lspconfig and remove conflicting configs

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,23 +1,2 @@
-local lsps = {
-    {"clangd"},
-    {"docker_language_server"},
-    {"cssls"},
-    {"rust_analyzer"},
-    {"tsc"},
-    {"tailwindcss"},
-    {"wc_language_server"},
-    {"oxlint"},
-    {"html", {
-        filetypes = { "html", "hbs", "handlebars", "html.handlebars"},
-    }},
-}
-
-vim.lsp.inlay_hint.enable(true);
-
-for _, lsp in pairs(lsps) do
-    local name, config = lsp[1], lsp [2]
-    vim.lsp.enable(name)
-    if config then
-        vim.lsp.config(name, config)
-    end
-end
+-- Treesitter
+vim.treesitter.language.register('html', {'handlebars', 'hbs'})
