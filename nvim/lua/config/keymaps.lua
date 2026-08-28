add3bf15629dca704a36c76d99aa561a71e95207
steeeeeeeeel <steel.cooper@proton.me>
2026-08-28 16:19:08 +1000

    Add keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,52 +1,49 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
-vim.keymap.set(
-    'n',
-    '<leader>w',
-    ':write<CR>',
+vim.keymap.set('n', '<leader>w', ':write<CR>',
     {
         desc = 'Write current file'
     }
 )
-vim.keymap.set(
-    'n',
-    '<leader>q',
-    ':quit<CR>',
+vim.keymap.set('n', '<leader>q', ':quit<CR>',
     {
         desc = 'Quit'
     }
 )
-vim.keymap.set(
-    'n',
-    '<Tab>',
-    '<cmd>:tabnext<CR>',
+vim.keymap.set('n', '<leader>wq', ':wq<CR>',
+    {
+        desc = 'Write and Quit'
+    }
+)
+vim.keymap.set('n', '<Tab>', '<cmd>:tabnext<CR>',
     {
         desc = 'Next Tab'
     }
 )
-vim.keymap.set(
-    'n',
-    '<S-Tab>',
-    '<cmd>:tabprevious<CR>',
+vim.keymap.set('n', '<S-Tab>', '<cmd>:tabprevious<CR>',
     {
         desc = 'Previous Tab',
     }
 )
-vim.keymap.set(
-    'i',
-    '<C-c>',
-    '<Esc>',
+vim.keymap.set('i','<C-c>','<Esc>',
     {
         desc = 'Exit insert mode',
         noremap = true
     }
 )
-vim.keymap.set(
-    'n',
-    '<CR>',
-    '<cmd>:let @/ = ""<CR>',
+vim.keymap.set('n', '<CR>', '<cmd>:let @/ = ""<CR>',
     {
         desc = 'Clear search highlighting when return input in normal mode',
         noremap = true
     }
 )
+vim.keymap.set('n', '<leader>i', function( )vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
+    {
+        desc = 'Toggle inlay hints',
+        noremap = true
+    }
+)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename,
+{
+    desc = "Rename lsp buffer"
+})
