049880032d44dd183946655b3f6b4930b1a6278b
steeeeeeeeel <steel.cooper@proton.me>
2026-09-08 13:30:03 +1000

    remove autoinstalls

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,18 +3,8 @@ return {
     lazy = false,
     build = ':TSUpdate',
     opts = {
-        sync_install = true,
-        auto_install = true,
-        ensure_installed = {
-            "tsx",
-            "toml",
-            "html",
-            "css",
-            "yaml",
-            "json",
-            "lua",
-            "javascript",
-            "python"
-        }
+        highlight = {
+            enable = true
+        },
     }
 }
